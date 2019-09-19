4b3fe1c4402273b681db01f5551d73ad60b5b42e
Kaynan Silva <kaynan_alves@hotmail.com>
2019-09-19 20:56:38 -0300

    Edit User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:show, :edit, :update]
   def dashboard
     @orders = current_user.orders
   end
@@ -8,4 +8,27 @@ class UsersController < ApplicationController
     current_user.orders
   end
 
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to dashboard_path, notice: 'User was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
